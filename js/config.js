bffed5ec47d368d5ab3a934b2884dcd63140bd63
MNPE-CUENG <mn-pe@chula.ac.th>
2026-07-21 16:27:20 +0700

    Update API_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // ไฟล์สำหรับเก็บค่าคอนฟิกส่วนกลางของระบบ
-const API_URL = "https://script.google.com/macros/s/AKfycbzyYTsH0oYMh6It4VGUdPZ-yJzOdaV-z-fdjp8-tKUKsV0MXRCc4ogybmfrhtzduhUJ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwZelPl9eeBJ4ScBQBVBvi_JEoThMAGTGbTJeJUDT8yTFVZ7GKoGDdQSP0dmt1EpUPL/exec";
