c2706e96bc312dad0d501bd8273c33217cf6521f
MNPE-CUENG <mn-pe@chula.ac.th>
2026-08-27 16:28:12 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // ไฟล์สำหรับเก็บค่าคอนฟิกส่วนกลางของระบบ
-const API_URL = "https://script.google.com/macros/s/AKfycbwil_sR92F8iLM3gmj70d_Vl2mPHPy6-6z5Huev46THbVapeKFwoJWEhLuhTzApD7if/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxX_AE6HDY17NiOXXX1dWOeZNj3mfqRefpgH36chf1PBRTXggsWUenm6SlBOpu2o0tc/exec";
